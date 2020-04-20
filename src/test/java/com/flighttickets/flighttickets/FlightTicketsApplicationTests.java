b82260bb7990408e05d4dd4c3ecebcc7e93c2145
unknown <nicoyamin@hotmail.com.ar>
2020-04-20 19:42:18 -0300

    Iniial commit

---
+++ src/test/java/com/flighttickets/flighttickets/FlightTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flighttickets.flighttickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
